5bee2bb89a1ba2cd85e63bc1c5811e8149daee2e
pixvenom <arryanmalhotra2@gmail.com>
2025-03-09 17:39:48 +0530

    First Commit

--- ADVENTURE GAME/Program.cs
+++ ADVENTURE GAME/Program.cs
@@ -15,6 +15,11 @@ namespace MyNamespace
             int choice;
             Console.WriteLine("Choose your character type: \n1. Warrior\n 2. Wizard\n 3. Archer");
             choice = Int32.Parse(Console.ReadLine());
+            if (!Int32.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid input, Enter integer");
+                return;
+            }
 
             if (choice == 1)
             {
@@ -41,6 +46,11 @@ namespace MyNamespace
             Console.WriteLine(
                 "Since you went for a hunt in the woods, you came across a tiger\n What would you choose?\n 1. Fight\n 2. Flight");
             int choice2 = Int32.Parse(Console.ReadLine());
+            if (!Int32.TryParse(Console.ReadLine(), out choice2))
+            {
+                Console.WriteLine("Invalid input, Provide integer value");
+                return;
+            }
             if (choice2 == 1)
             {
                 fight();
@@ -54,10 +64,15 @@ namespace MyNamespace
                 Console.WriteLine("Enter valid choice");
             }
 
-            static void fight()
+            void fight()
             {
                 Console.WriteLine("Good decision\n You have 2 weapons\n 1. Bow and Arrow\n 2. Sniper rifle");
                 int choice3 = Int32.Parse(Console.ReadLine());
+                if (!Int32.TryParse(Console.ReadLine(), out choice3))
+                {
+                    Console.WriteLine("Invalid input, Enter integer value");
+                    return;
+                }
                 if (choice3 == 1)
                 {
                     bow_arrow();
@@ -70,12 +85,11 @@ namespace MyNamespace
                 {
                     Console.WriteLine("Enter valid choice input");
                 }
-
-                static void bow_arrow()
+                void bow_arrow()
                 {
                     int arrows = 3;
                     Console.WriteLine("You have 3 arrows in total");
-                    while (arrows == 0)
+                    while (arrows > 0)
                     {
                         arrows--;
                         if (arrows == 2)
@@ -93,11 +107,11 @@ namespace MyNamespace
                     }
                 }
 
-                static void sniper_rifle()
+                void sniper_rifle()
                 {
                     int bullets = 10;
                     Console.WriteLine("You have 10 bullets in your gun\n Use them wisely");
-                    while (bullets == 0)
+                    while (bullets > 0)
                     {
                         bullets--;
                         if (bullets >= 5)
@@ -115,8 +129,7 @@ namespace MyNamespace
                     }
                 }
             }
-
-            static void flight()
+            void flight()
             {
                 Console.WriteLine("Coward!!!");
             }
@@ -129,6 +142,11 @@ namespace MyNamespace
                 "One fine day, you were taking a walk in a nearby garden, you witnessed a robbery\n Given your powers, you can cast a spell and stop the robbery then and there.\n Remember you are underage to use the magic and if caught, there will be serious consequences including some time in the prison\n Keeping this in mind, what would you thoose? 1. Use Magic \n 2. Call for Help");
 
             int choice3 = Int32.Parse(Console.ReadLine());
+            if (!Int32.TryParse(Console.ReadLine(), out choice3))
+            {
+                Console.WriteLine("Invalid input, Enter integer value");
+                return;
+            }
             if (choice3 == 1)
             {
                 use_magic();
@@ -141,12 +159,11 @@ namespace MyNamespace
             {
                 Console.WriteLine("Enter valid input");
             }
-
-            static void use_magic()
+            void use_magic()
             {
                 int magic = 5;
                 Console.WriteLine("You have 5 magic points");
-                while (magic == 0)
+                while (magic > 0)
                 {
                     if (magic >= 3)
                     {
@@ -162,8 +179,7 @@ namespace MyNamespace
                     }
                 }
             }
-
-            static void call_help()
+            void call_help()
             {
                 Console.WriteLine(
                     "Good decison ethics wise\n Go run home and come back once you are old enough to do something");
@@ -176,6 +192,11 @@ namespace MyNamespace
             Console.WriteLine("Imagine you are on your way to home where you find yourself surrounded by strange noises\n What would you do?\n 1. Explore and find out\n 2. Continue your journey by choosing a different path");
 
             int choice4 = Int32.Parse(Console.ReadLine());
+            if (!Int32.TryParse(Console.ReadLine(), out choice4))
+            {
+                Console.WriteLine("Invalid input, Enter integer value");
+                return;
+            }
             if (choice4 == 1)
             {
                 explore();
@@ -184,12 +205,16 @@ namespace MyNamespace
             {
                 different_path();
             }
-
-            static void explore()
+            void explore()
             {
                 Console.WriteLine("Good yet dangerous decision coz you don't know who can be there\n However you go to explore the area what you observe is that someone is being tied with chains\n You look around to find out there is been a ritual conducted\n What would you do? 1. Stop the ritual\n 2. Run as quickly as possible and report it to the higher authorities");
 
                 int choice5 = Int32.Parse(Console.ReadLine());
+                if (!Int32.TryParse(Console.ReadLine(), out choice5))
+                {
+                    Console.WriteLine("Invalid input, Enter integer value");
+                    return;
+                }
                 if (choice5 == 1)
                 {
                     stop_ritual();
@@ -202,13 +227,13 @@ namespace MyNamespace
                 {
                     Console.WriteLine("Enter valid decision number");
                 }
-
-                static void stop_ritual()
+                void stop_ritual()
                 {
                     int fire = 10;
                     Console.WriteLine("You have 10 fireballs to throw at the ritual and stop it right there\n Remember 4 fireballs are necessary to stop the ritual");
-                    while (fire==0)
+                    while (fire>0)
                     {
+                        fire--;
                         if (fire >= 5)
                         {
                             Console.WriteLine("You have 5 fireballs left\n hurry");
@@ -223,14 +248,12 @@ namespace MyNamespace
                         }
                     }
                 }
-
-                static void report()
+                void report()
                 {
                     Console.WriteLine("Good\n Get some proof and go to the concerned authorities");
                 }
             }
-
-            static void different_path()
+            void different_path()
             {
                 Console.WriteLine("Coward!!!, you are not worthy of the title");
             }
